5da65c8eb0823ae8f7cd672c52b8e16219989e24
Semma <semm1998>
2020-03-29 23:01:27 +0600

    Initial commit

---
+++ src/main/java/kg/attractor/lesson56lab/Lesson56labApplication.java
@@ -0,0 +1,13 @@
+package kg.attractor.lesson56lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson56labApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson56labApplication.class, args);
+    }
+
+}
